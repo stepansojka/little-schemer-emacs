8d6dc7b78a5d43c28c102e49714e4569a3356f22
Stepan Sojka <stepansojka@countermail.com>
2015-03-16 18:07:45 +1300

    start chapter1

---
+++ chapter1.el
@@ -0,0 +1,43 @@
+;; evaluate code samples with C-x e
+
+(atom 'atom)
+
+(atom 'turkey)
+
+(atom 1942)
+
+(atom '*abc$)
+
+(listp '(atom))
+
+(listp '(atom turkey or))
+
+(listp '(atom turkey) or)
+
+(atom '())
+
+(listp '())
+
+(car '(a b c))
+
+(car '((a b c) x y z))
+
+(car 'hotdog)
+
+(car '())
+
+(car '(((hotdogs)) (and) (pickle) relish))
+
+(car (car '(((hotdogs)) (and))))
+
+(cdr '(a b c))
+
+(cdr '((a b c) x y z))
+
+(cdr '(hamburger))
+
+(cdr '((x) t r))
+
+(cdr 'hotdogs)
+
+(cdr '())
